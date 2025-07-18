08d56d2ea71bc25daeab55b7306029e03bd939d3
hung <hung@HUNG165>
2025-07-18 13:51:02 +0700

    add backup delivery site

---
+++ backup_deliverysite.js
@@ -0,0 +1,23 @@
+          //  props.formState["id"] = Math.floor(Math.random() * 10000);
+          //     const keys = Object.keys(props.formState);
+          //     const newObj = Object.fromEntries(
+          //       keys.map(key => [key.toLowerCase(), props.formState[key]])
+          //      );
+          //      //newObj.id = Math.floor(Math.random() * 1000);
+          //       const options = {
+          //           headers: {
+          //               'accept': 'application/json',
+          //               'content-type': 'application/json',
+          //               //'x-api-key': 'IMcTHO6mtABYDAMYUMsyYg.Lt3Rg48o9l-zanIIJacDZJldsWz8ycOc8yGdm5PbCag'
+          //               'odp-object': (window as any).formName 
+          //           }
+          //       };
+ 
+          //       axios.post((window as any).submitUrl || 'https://localhost:7207/api/formsubmission', newObj, options)
+          //       .then(response => {
+          //           alert('Form submitted successfully!');
+          //       })
+          //       .catch(error => {
+          //           console.error('Error submitting form:', error);
+          //       });
+          //   }}>
